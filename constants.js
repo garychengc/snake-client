237d6ccb96cd507b4349e66563c4056446aa5754
Gary Chen <garychen.whc@gmail.com>
2019-08-28 03:28:18 +0000

    snake game rev 1

--- constants.js
+++ constants.js
@@ -1,12 +1,7 @@
 const IP = '10.0.2.15';
 const PORT = 50541;
 const NAME = 'Yoo';
-const MOVE_UP_KEY = 'w';
-const MOVE_DOWN_KEY = 's';
-const MOVE_RIGHT_KEY = 'd';
-const MOVE_LEFT_KEY = 'a';
+const MOVE_KEY = {UP: 'w', DOWN: 's', RIGHT: 'd', LEFT: 'a'};
 const MESSAGE = {m: 'sup'};
 
-
-
-module.exports = {IP, PORT, NAME, MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MOVE_LEFT_KEY, MESSAGE};
+module.exports = {IP, PORT, NAME, MOVE_KEY, MESSAGE};
--- input.js
+++ input.js
@@ -1,7 +1,8 @@
-const {MOVE_KEY, MESSAGE} = require('./constants')
+const { MOVE_KEY, MESSAGE } = require("./constants");
 let connection;
 let interval;
 let direction;
+let command;
 
 const setupInput = function(conn) {
   connection = conn;
@@ -15,18 +16,22 @@ const setupInput = function(conn) {
 
 const handleUserInput = function() {
   const stdin = process.stdin;
-  direction = MOVE_KEY.left;
-  let command;
+  direction = MOVE_KEY.LEFT;
 
   stdin.on("data", key => {
+    if(key === direction) return;
 
-    if ((key === MOVE_KEY.up && direction !== MOVE_KEY.down) || (key === MOVE_KEY.down && direction !== MOVE_KEY.up) ||  (key === MOVE_KEY.left && direction !== MOVE_KEY.right) || (key === MOVE_KEY.right && direction !== MOVE_KEY.left)) {
+    if (
+      (key === MOVE_KEY.UP && direction !== MOVE_KEY.DOWN) ||
+      (key === MOVE_KEY.DOWN && direction !== MOVE_KEY.UP) ||
+      (key === MOVE_KEY.LEFT && direction !== MOVE_KEY.RIGHT) ||
+      (key === MOVE_KEY.RIGHT && direction !== MOVE_KEY.LEFT)) {
       clearInterval(interval);
       direction = key;
 
       for (let value in MOVE_KEY) {
         if (MOVE_KEY[value] === key) {
-          command = value;
+          command = value.toLowerCase();
         }
       }
       interval = setInterval(() => {
@@ -38,9 +43,8 @@ const handleUserInput = function() {
       connection.write(`Say: ${MESSAGE[key]}`);
     }
 
-
     if (key === "\u0003") {
-      console.log("Thanks!");
+      console.log("Thanks for playing this amazing game!");
       process.exit();
     }
   });
